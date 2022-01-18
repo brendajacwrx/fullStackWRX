6613385868223922d3e9c3dbd7be26208156fa1a
Brenda J Anderson <brendaja@computerwrx.com>
2022-01-17 21:41:29 -0500

    assignments to projects

---
+++ projects/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Directorey</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1 class="hero">Project Listings</h1>
+        <nav>
+            <ul>
+               <li><a href=""></a>Home</li>
+               <li><a href=""></a>About</li>
+               <li><a href=""></a>Photography</li>
+               <li><a href="">Code</a></li> 
+            </ul>
+        </nav>
+    </header>
+    <main class="main" id="main">
+
+    </main> <!-- end main -->
+    <footer>
+        
+    </footer>
+</body>
+</html>
